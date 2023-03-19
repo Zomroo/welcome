a42f1c82e93010acd8940a8bbffa0a27d0b3ce9e
Zomroo <93994060+Zomroo@users.noreply.github.com>
2023-03-20 02:00:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,34 +13,42 @@ BOT_TOKEN = '6145559264:AAEkUH_znhpaTdkbnndwP1Vy2ppv-C9Zf4o'
 app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 # Define the font for the welcome message
-font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSerif.ttf', 40)
+font_path = '/usr/share/fonts/truetype/dejavu/DejaVuSerif.ttf' # Path to your custom font file
+font_size = 70
+font_color = (255, 255, 255)
+font = ImageFont.truetype(font_path, font_size)
 
-def welcome(client, message):
+# Define the welcome image URL
+image_url = 'https://i.postimg.cc/Hsggt1hn/photo-2023-03-20-01-40-46-7212352388177380352.png'
+
+def generate_welcome_image(name, username, user_id):
+    # Download the welcome image
+    response = requests.get(image_url)
+    with Image.open(requests.get(image_url, stream=True).raw) as image:
+        # Open the image and add the text
+        draw = ImageDraw.Draw(image)
+        draw.text((120, 250), f'Welcome {name}!', fill=font_color, font=font)
+        draw.text((120, 350), f'Username: @{username}', fill=font_color, font=font)
+        draw.text((120, 450), f'ID: {user_id}', fill=font_color, font=font)
+
+        # Save the modified image
+        modified_image_path = 'images/welcome_modified.jpg'
+        image.save(modified_image_path)
+        
+        return modified_image_path
+@app.on_message(filters.new_chat_members)
+def handle_new_chat_members(client, message):
     user = message.new_chat_members[0]
     name = user.first_name
     username = user.username
     user_id = user.id
 
-    # Download the welcome image
-    image_url = 'https://i.postimg.cc/Hsggt1hn/photo-2023-03-20-01-40-46-7212352388177380352.png'
-    response = requests.get(image_url)
-    with Image.open(requests.get(image_url, stream=True).raw) as image:
-        # Open the image and add the text
-        draw = ImageDraw.Draw(image)
-        draw.text((100, 100), f'Welcome {name}!', fill='white', font=font)
-        draw.text((100, 200), f'Username: {username}', fill='white', font=font)
-        draw.text((100, 300), f'ID: {user_id}', fill='white', font=font)
+    # Generate the welcome image
+    modified_image_path = generate_welcome_image(name, username, user_id)
 
-        # Save the modified image
-        image.save('welcome_modified.jpg')
-
-        # Send the modified image as a reply to the welcome message
-        with open('welcome_modified.jpg', 'rb') as f:
-            client.send_photo(chat_id=message.chat.id, photo=f, caption=f'Hello {name}! Welcome to the group.')
-
-@app.on_message(filters.new_chat_members)
-def handle_new_chat_members(client, message):
-    welcome(client, message)
+    # Send the modified image as a reply to the welcome message
+    with open(modified_image_path, 'rb') as f:
+        client.send_photo(chat_id=message.chat.id, photo=f)
 
 @app.on_message(filters.command('start'))
 def start(client, message):

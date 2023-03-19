1f25aeaca5c721e16fd607a74375f5a4dfae7123
Zomroo <93994060+Zomroo@users.noreply.github.com>
2023-03-20 03:13:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,77 +2,41 @@ import os
 import requests
 from PIL import Image, ImageDraw, ImageFont
 import pyrogram
-import pyfiglet
+from pyrogram import Client, filters
 
-# Define the path to your custom font file
-font_path = os.path.join('fonts', 'CROWNT.ttf')
 
 API_ID = 15849735 # Your API ID
 API_HASH = 'b8105dc4c17419dfd4165ecf1d0bc100' # Your API Hash
 BOT_TOKEN = '6145559264:AAEkUH_znhpaTdkbnndwP1Vy2ppv-C9Zf4o'
 
 # Create a Pyrogram client
-app = pyrogram.Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 # Define the font for the welcome message
-font_path = '/usr/share/fonts/truetype/dejavu/DejaVuSerif.ttf'
-font_size = 70
+font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSerif.ttf', 40)
 
-# Define the welcome image URL
-image_url = 'https://i.postimg.cc/Hsggt1hn/photo-2023-03-20-01-40-46-7212352388177380352.png'
-
-def generate_welcome_image(name, username, user_id):
-    # Download the welcome image
-    response = requests.get(image_url)
-    with Image.open(requests.get(image_url, stream=True).raw) as image:
-        # Open the image and add the text
-        draw = ImageDraw.Draw(image)
-
-        # Use simple font styles
-        name_text = f'Welcome {name}!'
-        username_text = f'Username: {username}'
-        user_id_text = f'ID: {user_id}'
-
-
-        # Define font and color for the text
-        name_font = ImageFont.truetype(font_path, font_size)
-        username_font = ImageFont.truetype(font_path, font_size-20)
-        user_id_font = ImageFont.truetype(font_path, font_size-20)
-        font_color = (241, 196, 15)
-
-        # Draw the text on the image
-        draw.text((120, 250), name_text, fill=font_color, font=name_font)
-        draw.text((120, 350), username_text, fill=font_color, font=username_font)
-        draw.text((120, 450), user_id_text, fill=font_color, font=user_id_font)
-
-        # Save the modified image
-        modified_image_path = 'images/welcome_modified.jpg'
-        image.save(modified_image_path)
-
-        return modified_image_path
-
-
-@app.on_message(pyrogram.filters.new_chat_members)
-def handle_new_chat_members(client, message):
+def welcome(client, message):
     user = message.new_chat_members[0]
     name = user.first_name
     username = user.username
     user_id = user.id
 
-    # Generate the welcome image
-    modified_image_path = generate_welcome_image(name, username, user_id)
+    # Download the welcome image
+    image_url = 'https://i.postimg.cc/Hsggt1hn/photo-2023-03-20-01-40-46-7212352388177380352.png'
+    response = requests.get(image_url)
+    with Image.open(requests.get(image_url, stream=True).raw) as image:
+        # Open the image and add the text
+        draw = ImageDraw.Draw(image)
+        draw.text((100, 100), f'Welcome {name}!', fill='white', font=font)
+        draw.text((100, 200), f'Username: {username}', fill='white', font=font)
+        draw.text((100, 300), f'ID: {user_id}', fill='white', font=font)
 
-    # Send the modified image as a reply to the welcome message
-    with open(modified_image_path, 'rb') as f:
-        client.send_photo(chat_id=message.chat.id, photo=f)
+        # Save the modified image
+        image.save('welcome_modified.jpg')
 
         # Send the modified image as a reply to the welcome message
         with open('welcome_modified.jpg', 'rb') as f:
             client.send_photo(chat_id=message.chat.id, photo=f, caption=f'Hello {name}! Welcome to the group.')
-        
-@app.on_message(pyrogram.filters.command('start'))
-def start(client, message):
-    client.send_message(chat_id=message.chat.id, text="Hello! I'm a welcome bot.")
 
 
 # Start the client
